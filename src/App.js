c668a1dada9b0342a26f1e5ae2c0556239d11c16
jon-tschida <jon.tschida@yahoo.com>
2023-03-31 17:20:14 -0400

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Weather from './components/Weather';
-import Settings from './components/Settings';
+// import Settings from './components/Settings';
 
 // Durham NC API call, we'll want to get the lat and long from the user though down the road. 
 // https://api.openweathermap.org/data/2.5/weather?lat=35.994034&lon=-78.898621&units=imperial&appid=2a8ab662e8539e2cb45726e6080084e6
@@ -9,15 +9,18 @@ import Settings from './components/Settings';
 
 
 export default function App() {
-  const [locationData, setLocationData] = React.useState()
   const [loaded, setLoaded] = React.useState(false)
   const [weatherData, setWeatherData] = React.useState()
   const [loadingWeatherData, setLoadingWeatherData] = React.useState(true);
   const [loadingDailyWeatherData, setLoadingDailyWeatherData] = React.useState(true)
   const [dailyWeatherData, setDailyWeatherData] = React.useState()
+  const [locationData, setLocationData] = React.useState()
   
   
-  // API call function, bascially copy pasted into our Header as well, using the lat and long from our location api to get weather for that location. 
+  
+  // UseEffect with loaded in the dependancy array 
+  React.useEffect(()=>{ 
+      // API call function, bascially copy pasted into our Header as well, using the lat and long from our location api to get weather for that location. 
   async function fetchWeatherData() {
     setLoadingWeatherData(true);
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${locationData.locations[0].referencePosition.latitude.toFixed(5)}&lon=${locationData.locations[0].referencePosition.longitude.toFixed(5)}&units=imperial&appid=2a8ab662e8539e2cb45726e6080084e6`);
@@ -35,16 +38,12 @@ export default function App() {
     setDailyWeatherData(json);
     setLoadingDailyWeatherData(false);
   }
-  
-  
-  // UseEffect with loaded in the dependancy array 
-  React.useEffect(()=>{ 
 
       if(loaded) {
         fetchDailyWeatherData();
         fetchWeatherData();
       }   
-  }, [loaded])
+  }, [loaded]) // eslint-disable-next-line react-hooks/exhaustive-deps
 
   
   // Debugging stuff:
